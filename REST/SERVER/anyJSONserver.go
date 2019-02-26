d6f3c23ed68acaa3ac7346ddc1dc029eab8c684d
Jaideep Kekre <jaideepkekre@gmail.com>
2019-02-26 16:39:33 +0530

    added sample rest server

---
+++ REST/SERVER/anyJSONserver.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	// "fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/sample", Sample).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8001", router))
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func Sample(w http.ResponseWriter, r *http.Request) {
+
+	var f interface{}
+	body, _ := ioutil.ReadAll(r.Body)
+	_ = json.Unmarshal(body, &f)
+	m := f.(map[string]interface{})
+	something, _ := json.Marshal(m)
+	print(something)
+}
